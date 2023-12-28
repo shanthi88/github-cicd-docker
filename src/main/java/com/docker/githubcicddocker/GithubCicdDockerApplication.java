2b0eca770f6a8f71b297c0811cd210581dabc186
shanthi <shanthidurga88@gmail.com>
2023-12-28 13:22:27 +0530

    github cicd docker initial commit

---
+++ src/main/java/com/docker/githubcicddocker/GithubCicdDockerApplication.java
@@ -0,0 +1,13 @@
+package com.docker.githubcicddocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubCicdDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCicdDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/docker/githubcicddocker/GithubCicdDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker.githubcicddocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubCicdDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
